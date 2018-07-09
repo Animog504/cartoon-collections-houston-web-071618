e0e308b284590268a225190054901b8c2efe9dd3
Scott Ungchusri <animoogle504@gmail.com>
2018-07-09 00:27:33 +0000

    automatically backed up by learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -23,7 +23,9 @@ def long_planeteer_calls(calls)# code an argument here
   return longBoi
 end
 
-def find_the_cheese# code an argument here
+def find_the_cheese(itemList)# code an argument here
   # the array below is here to help
   cheese_types = ["cheddar", "gouda", "camembert"]
+  
+  itemList.map {|x| x.contains
 end

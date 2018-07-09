58f2937183aef4c5f8767e699ab651a75044cf49
Scott Ungchusri <animoogle504@gmail.com>
2018-07-09 00:43:34 +0000

    automatically backed up by learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -27,5 +27,7 @@ def find_the_cheese(itemList)# code an argument here
   # the array below is here to help
   cheese_types = ["cheddar", "gouda", "camembert"]
   
-  itemList.map {|x| cheese_types.include?(x)}
+  itemList.each_with_index |item,index| cheese_types.include?(item)
+  puts itemList(index)
+  
 end
